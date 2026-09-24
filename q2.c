c7f91540ddfb0b6e8650ae88ce4e004c9e9c063e
tanishkakala219-cmyk <tanishkakala219@gmail.com>
2026-09-24 09:17:38 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+int num1, num2;
+int sum, diff, product, quotient;
+printf("enter two numbers");
+scanf("%d","%d",&num1,&num2);
+{
+sum = num1 + num2;
+diff = num1 - num2;
+product = num1 * num2;
+quotient = num1 / num2;
+printf("Sum=", sum);    
+printf("diff=",diff);
+printf("product=",product);
+printf("quotient="quotient);
+return 0;
+}
